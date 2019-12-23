dadc2a3952dbe1a94cb653d2d02c975572397675
Selistral <NeksusVita404@mail.ru>
2019-12-23 12:24:17 +0300

    Start

---
+++ gb-tracker/console/controllers/console.php
@@ -0,0 +1,12 @@
+<?
+namespace console\controllers;
+
+use Yii;
+
+class console extends \yii\console\Controller /// Not working....
+{                                             ///Why?
+    public function actionGreatings()
+    {
+        print "Hello, world";
+    }
+}
---
+++ gb-tracker/frontend/views/site/task.php
@@ -0,0 +1,1 @@
+<h1>This Task</h1>
